9f0ae4ab9c4403110a006e2e771ddbefa08c8d86
fm Bushry <fmbushry14@gmail.com>
2024-03-06 23:56:22 +0530

    Add project files.

---
+++ Searching String/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string songlyrics = "my name is is billa";
+Console.WriteLine(songlyrics.Contains("billa"));
+Console.WriteLine(songlyrics.Contains("balla"));
